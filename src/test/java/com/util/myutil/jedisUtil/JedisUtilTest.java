7b462b48309ff5069cd65557ea86e6ec1c5a1219
liyuhang <liyuhang@kuangjie.net>
2018-12-13 10:01:15 +0800

    thread demo up up

---
+++ src/test/java/com/util/myutil/jedisUtil/JedisUtilTest.java
@@ -0,0 +1,17 @@
+package com.util.myutil.jedisUtil;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author:
+ * @Date: 2018/12/12 14:45
+ * @Description:
+ */
+public class JedisUtilTest {
+
+    @Test
+    public void getJedis() {
+    }
+}
